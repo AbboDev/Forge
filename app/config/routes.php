52924f2093661fcccfad91018f97c0c71e79f5a7
Abbo <digital.crystalware@gmail.com>
2019-11-02 20:06:51 +0100

    Better routes generator

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,18 +2,28 @@
 
 use Phalcon\Mvc\Micro\Collection as MicroCollection;
 
-$manufacturerCollection = new MicroCollection();
-$manufacturerCollection->setHandler('Forge\Controllers\ManufacturerController', true);
-$manufacturerCollection->setPrefix('/manufacturer');
+$formats = implode('|', $config->api->formats->toArray());
 
-$manufacturerCollection->get('/', 'index');
+$manufacturerCollection = call_user_func(function($formats) use ($config) {
+    $collection = new MicroCollection();
+    $collection->setHandler('Forge\Controllers\ManufacturerController', true);
+    $collection->setPrefix('/manufacturer');
 
-$manufacturerCollection->get("/{name:([a-zA-Z0-9\_\-]+)}/?{format:(json|xml|te?xt|x?html|m(ark)?d(own)?|ya?ml)?}", 'getSingleByName');
-$manufacturerCollection->get("/{name:([a-zA-Z0-9\_\-]+)}/info/?{format:(json|xml|te?xt|x?html|m(ark)?d(own)?|ya?ml)?}", 'getInfoByName');
+    $collection->get('/', 'index');
 
-$manufacturerCollection->get("/{id:([0-9]+)}/?{format:(json|xml|te?xt|x?html|m(ark)?d(own)?|ya?ml)?}", 'getSingleById');
-$manufacturerCollection->get("/{id:([0-9]+)}/info/?{format:(json|xml|te?xt|x?html|m(ark)?d(own)?|ya?ml)?}", 'getInfoById');
+    $routes = array(
+        '{name:([a-zA-Z0-9\_\-]+)}' => 'getSingleByName',
+        '{name:([a-zA-Z0-9\_\-]+)}/info' => 'getInfoByName',
+        '{id:([0-9]+)}' => 'getSingleById',
+        '{id:([0-9]+)}/info' => 'getInfoById',
+        $config->api->all_alias => 'getList',
+    );
 
-$manufacturerCollection->get("/@/?{format:(json|xml|te?xt|x?html|m(ark)?d(own)?|ya?ml)?}", 'getList');
+    foreach ($routes as $path => $method) {
+        $collection->get("/{$path}/?{format:({$formats})?}", $method);
+    }
+
+    return $collection;
+}, $formats);
 
 $app->mount($manufacturerCollection);
