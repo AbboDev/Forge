46ed9847e36eab665004baca8666e9f596cb3178
Abbo <digital.crystalware@gmail.com>
2019-11-09 15:19:52 +0100

    Apply format control only with API route

--- app/config/events.php
+++ app/config/events.php
@@ -12,53 +12,57 @@ $eventsManager->attach(
         $return = $app->getReturnedValue();
         $params = $app->router->getParams();
 
-        $format = (array_key_exists('format', $params)) ? $params['format'] : null;
+        if (preg_match("/\/api\/v[0-9]+\//", $app->router->getRewriteUri())) {
+            $format = (array_key_exists('format', $params)) ? $params['format'] : null;
 
-        switch ($format) {
-            case 'json':
-                $content_type = 'application/json';
-                break;
+            switch ($format) {
 
-            case 'md':
-            case 'markdown':
-                $content_type = 'text/x-markdown';
-                // $content_type = 'text/markdown';
-                break;
+                case 'md':
+                case 'markdown':
+                    $content_type = 'text/x-markdown';
+                    // $content_type = 'text/markdown';
+                    break;
 
-            case 'txt':
-            case 'text':
-                $content_type = 'text/plain';
-                break;
+                case 'txt':
+                case 'text':
+                    $content_type = 'text/plain';
+                    break;
 
-            case 'yml':
-            case 'yaml':
-                $content_type = 'application/x-yaml';
-                // $content_type = 'text/yaml';
-                break;
+                case 'yml':
+                case 'yaml':
+                    $content_type = 'application/x-yaml';
+                    // $content_type = 'text/yaml';
+                    break;
 
-            case 'xhtml':
-                $content_type = 'application/xhtml+xml';
-                break;
+                case 'xhtml':
+                    $content_type = 'application/xhtml+xml';
+                    break;
 
-            case 'xml':
-                $content_type = 'text/xml';
-                // $content_type = 'application/xml';
-                break;
+                case 'xml':
+                    $content_type = 'text/xml';
+                    // $content_type = 'application/xml';
+                    break;
 
-            case null:
-            case 'html':
-            default:
-                $content_type = 'text/html';
-                break;
-        }
+                case 'html':
+                    $content_type = 'text/html';
+                    break;
 
-        $app->response->setContentType($content_type, 'UTF-8');
+                case null:
+                case 'json':
+                default:
+                    $content_type = 'application/json';
+                    break;
+            }
 
-        if (is_array($return)) {
-            $app->response->setContent(json_encode($return));
-        } else {
-            // Unexpected response
-            throw new Exception('Bad Response');
+            $app->response->setContentType($content_type, 'UTF-8');
+
+            if (is_array($return)) {
+                $app->response->setContent(json_encode($return));
+            } elseif (!strlen($return)) {
+                $app->response->setStatusCode('204', 'No Content');
+            } else {
+                throw new Exception('Bad Response');
+            }
         }
 
         $app->response->send();
